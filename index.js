b6754620778fa5f6dc6a3725c07b7a203c902fa4
Md Shahbaz Alam <shahbazalam75508@gmail.com>
2021-10-25 09:25:05 +0530

    Commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const { Octokit } = require("@octokit/rest");
+const core = require('@actions/core');
+const { context, GitHub } = require('@actions/github');
+const token = core.getInput('repo-token');
+const octokit = new GitHub(token);
+
+octokit.issues.createComment({
+    issue_number: 1,
+    body: 'Hi,First program',
+    ...context.repo,
+});
