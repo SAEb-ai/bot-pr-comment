7c44f0c6bbb252661f8e2ea288bea838d29afa6d
Md Shahbaz Alam <shahbazalam75508@gmail.com>
2021-10-25 14:22:42 +0530

    Commit

--- dist/index.js
+++ dist/index.js
@@ -8445,7 +8445,8 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-const octokit = github.getOctokit(GITHUB_TOKEN);
+const octokit = github.getOctokit(_TOKEN);
+console.log(GITHUB_TOKEN);
 
 const { context = {} } = github;
 const { pull_request } = context.payload;
