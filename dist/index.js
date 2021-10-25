a803f209beefb58a0483972ed3fb1ca8aeb50568
Md Shahbaz Alam <shahbazalam75508@gmail.com>
2021-10-25 14:34:50 +0530

    Commit

--- dist/index.js
+++ dist/index.js
@@ -8452,7 +8452,7 @@ const { context = {} } = github;
 const { pull_request } = context.payload;
 
 async function run() {
-  await octokit.issues.createComment({
+  await octokit.rest.issues.createComment({
     ...context.repo,
     issue_number: pull_request.number,
     body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
